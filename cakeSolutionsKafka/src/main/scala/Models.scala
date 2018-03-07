8d5217e4cf67b77a0581cec24f87a25a9e9ce941
Alberto Coletta <alberto.coletta@banksealer.com>
2018-03-07 12:50:00 +0100

    Add cake models

---
+++ cakeSolutionsKafka/src/main/scala/Models.scala
@@ -0,0 +1,24 @@
+package cakekafka
+
+import io.buildo.enumero.annotations.enum
+import java.time.LocalDateTime
+
+
+case class MessageTest(
+  myId: Int,
+  myString: String,
+  myEnum: RandomEnum,
+  myDateTime: LocalDateTime,
+  myDouble: Double,
+  myFeature: Map[String, Int]
+)
+
+@enum trait RandomEnum {
+  object First
+  object Second
+  object Third
+}
+
+object Utils {
+  def topic: String = "messageTestTopic"
+}
